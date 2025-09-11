dd96bd5bfb01fc1bb915ec1d982a8e8d82d8bc69
Sarthak-uptiq <sarthak@uptiq.ai>
2025-09-12 03:02:47 +0530

    changes

--- services/employee_crud_service/src/router/emp_details_routes.ts
+++ services/employee_crud_service/src/router/emp_details_routes.ts
@@ -3,6 +3,7 @@ import { validateGetRequest } from "../middleware/validateGet.ts";
 import { validateUpdateRequest } from "../middleware/validateUpdate.ts";
 import { authenticateRequest } from "../middleware/authenticate.ts";
 import { validateEmpStatus } from "../middleware/validateStatus.ts";
+import { errorHandler } from "../middleware/errorHandler.ts";
 import {
   getAllDetails,
   updateDetails,
@@ -15,25 +16,29 @@ empDetailRouter.get(
   "/get-all-details",
   authenticateRequest,
   validateGetRequest,
-  getAllDetails
+  getAllDetails,
+  errorHandler
 );
 empDetailRouter.put(
   "/update-emp-details",
   authenticateRequest,
   validateUpdateRequest,
-  updateDetails
+  updateDetails, 
+  errorHandler
 );
 empDetailRouter.put(
   "/update-emp-status",
   authenticateRequest,
   validateEmpStatus,
-  updateFlags
+  updateFlags,
+  errorHandler
 );
 empDetailRouter.put(
   "/ack-policies",
   authenticateRequest,
   validateEmpStatus,
-  updateFlags
+  updateFlags,
+  errorHandler
 );
 
 export default empDetailRouter;
